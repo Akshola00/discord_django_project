656f179d4d693ab223622dc2e90f55a05e72a9ae
Akshola00 <akinniyishola07@gmail.com>
2024-01-03 13:37:33 +0100

    first commit

---
+++ studybud_customedUser/base/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Room
+from .models import Topic
+from .models import Message
+from .models import User
+
+# Register your models here.
+
+admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)
+admin.site.register(User)
