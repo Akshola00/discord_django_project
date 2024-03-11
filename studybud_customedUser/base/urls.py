f7a80a8d13eed27fca64afbfdc096d90d3bbcc3e
Akshola00 <akinniyishola07@gmail.com>
2024-03-11 21:17:47 +0100

    progress

--- studybud_customedUser/base/urls.py
+++ studybud_customedUser/base/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('logout', views.logoutuser, name="logout"),
 
     path('', views.home, name="home"),
+    
     path('room/<str:pk>/', views.room, name="room"),
     path('profile/<str:pk>/', views.userProfile, name="user-profile"),
     
